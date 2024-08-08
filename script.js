c26b5faac0afc40ad59e394d4016d1685854716d
joseio72 <jossorio-pro@outlook.com>
2024-08-07 21:44:37 -0400

    commmet out code

---
+++ script.js
@@ -0,0 +1,22 @@
+//Scroll Index
+//const getDocumentHeight = () => {
+//return Math.max(
+//document.body.scrollHeight, document.documentElement.scrollHeight,
+//document.body.offsetHeight, document.documentElement.offsetHeight,
+//document.body.clientHeight, document.documentElement.clientHeight
+//);
+//};
+//document.addEventListener('DOMContentLoaded', () => {
+//const height = getDocumentHeight();
+//const scrollProgress = document.getElementById('progressBar');
+//scrollProgress.max = height;
+//console.log(height)
+//console.log(scrollProgress)
+// window.addEventListener('scroll', () => {
+//  const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+// scrollProgress.value = (scrollTop / height) * 100;
+//});
+//});
+//
+
+
