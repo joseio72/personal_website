1a40f67e2c6a0aa0763861c59f7f232d4efab789
joseio72 <jossorio-pro@outlook.com>
2024-08-08 15:50:23 -0400

    before branching

--- script.js
+++ script.js
@@ -18,5 +18,30 @@
 //});
 //});
 //
+//
+//  <p clas="details_text">
+//
+function numberToWords(num) {
+  const words = ["zero", "one", "two", "three",
+    "four", "five", "six", "seven", "eight", "nine", "ten",
+    "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+    "eighteen", "nineteen"];
+  const tens = ["twenty", "thirty",
+    "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 
+  if (num < 20) return words[num];
+  if (num < 100) return tens[Math.floor(num / 10)] + (num % 10 !== 0 ? "-" + words[num % 10] : "");
+  if (num < 1000) return words[Math.floor(num / 100)] + " hundred" + (num % 100 !== 0 ? " and " + numberToWords(num % 100) : "");
 
+  return num;
+}
+
+function updateExperience() {
+  const startYear = 2022;
+  const currentYear = new Date().getFullYear();
+  const yearsOfExperience = currentYear - startYear;
+  const yearsInWords = numberToWords(yearsOfExperience);
+  document.getElementById("inner_years").textContent = yearsInWords;
+}
+
+window.onload = updateExperience;
